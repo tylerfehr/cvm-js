4492f0afc333f4a2f8abbd8b7bebc4b5941a1c30
Tyler Fehr <tfehr@proton.me>
2024-05-18 14:32:28 -0400

    simplify calculateBufferSize

--- src/util.ts
+++ src/util.ts
@@ -2,17 +2,7 @@
  * Calculates the threshold (thresh or bufferSize) value for the F0-Estimator algorithm.
  */
 export const calculateBufferSize = (epsilon: number, delta: number, streamSize: number): number => {
-  if (epsilon <= 0 || epsilon >= 1) {
-    throw new Error("Epsilon must be between 0 and 1 (exclusive)");
-  }
-
-  if (delta <= 0 || delta >= 1) {
-    throw new Error("Delta must be between 0 and 1 (exclusive)");
-  }
-
-  if (streamSize <= 0) {
-    throw new Error("Stream size must be a positive integer");
-  }
-
-  return Math.ceil((12 / Math.pow(epsilon, 2)) * Math.log2((8 * streamSize) / delta));
-}
+  return Math.ceil(
+    (12 / Math.pow(epsilon, 2)) * Math.log2((8 * streamSize) / delta)
+  );
+};
